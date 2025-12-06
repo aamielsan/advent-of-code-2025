2b20479acb968fd8a17aee781ae4cdf9ea3dd6a3
Adrian Sanchez <aamiel.sanchez@gmail.com>
2025-12-06 09:50:58 +0100

    fix: day 6 brute force

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,133 @@
+fun main() {
+    fun part1(input: List<String>): Long {
+        val operations = input.takeLast(1).flatMap { it.split(" ").filter(String::isNotEmpty).map(String::trim) }
+        var count = 0
+        return input
+            .dropLast(1)
+            .map {
+                val arr = it
+                    .split(" ")
+                    .filter(String::isNotEmpty)
+                    .map(String::trim)
+                    .map(String::toLong)
+                    .withIndex()
+
+                println("arr: ${arr.map { it.value }}")
+                count = arr.count()
+                arr
+            }
+            .foldIndexed(MutableList<Long>(count) { 0 }) { idx, acc, line ->
+                line.forEach { (index, number) ->
+                    println("index: $index, number: $number, $acc")
+                    when {
+                        idx == 0 -> {
+                            acc[index] = number
+                        }
+
+                        else -> {
+                            acc[index] = when (operations[index]) {
+                                "+" -> acc[index] + number
+                                "*" -> acc[index] * number
+                                else -> error("Unknown operation")
+                            }
+                        }
+                    }
+                }
+                acc
+            }
+            .sum()
+    }
+
+    fun part2(input: List<String>): Long {
+        val splits: List<ClosedRange<Int>> =
+            input
+                .last()
+                .foldIndexed(emptyList<Int>()) { index, acc, s ->
+                    when {
+                        s == ' ' -> acc
+                        else -> acc + index
+                    }
+                }
+                .zipWithNext()
+                .map { it.first until it.second - 1 }
+                .let {
+                    val last = it.last().last + 2 until input.maxBy(String::length).length
+                    it + listOf(last)
+                }
+                .also(::println)
+
+        val operations =
+            input
+                .takeLast(1)
+                .flatMap {
+                    it
+                        .split(" ")
+                        .filter(String::isNotEmpty)
+                        .map(String::trim)
+                }
+
+        val lines: List<List<String>> =
+            input
+                .dropLast(1)
+                .map { line ->
+                    splits.map { range ->
+                        line.substring(
+                            range.start,
+                            minOf(range.endInclusive + 1, line.length),
+                        )
+                    }
+                }
+
+        // transpose, get all numbers in same column
+        val transposed =
+            (0 until lines.first().size).map { j ->
+                (0 until lines.size).map { i ->
+                    lines[i][j]
+                }
+            }
+
+        // for each row, transpose again but for the digits + pad
+        val result =
+            transposed
+                .map { row ->
+                    val maxLength = row.maxOf { it.length }
+                    val paddedRows = row.map { number ->
+                        if (number.length < maxLength) {
+                            val padded = number.padEnd(maxLength, ' ')
+                            padded
+                        } else {
+                            number
+                        }
+                    }
+
+                    val columns = paddedRows.first().length
+                    val rows = paddedRows.size
+
+                    (0 until columns).map { j ->
+                        (0 until rows).fold("") { acc, i ->
+                            acc + paddedRows[i][j]
+                        }
+                    }
+                }
+                // parse to long
+                .map { row ->
+                    row.map(String::trim).map(String::toLong)
+                }
+                .also(::println)
+                .mapIndexed { index, row ->
+                    val operation = operations[index]
+                    when (operation) {
+                        "+" -> row.sum()
+                        "*" -> row.fold(1L) { acc, n -> acc * n }
+                        else -> error("Unknown operation")
+                    }
+                }
+                .sum()
+
+        return result
+    }
+
+    val input = readInput("Day06")
+//    println(part1(input))
+    println(part2(input))
+}
